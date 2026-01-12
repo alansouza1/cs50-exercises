ce04be113481e373672579ae613249431fd14317
alansouza1 <alansouza4001@gmail.com>
2026-01-12 16:24:20 -0300

    Add left align pyramid exercise

---
+++ week-1-c/left-align-pyramid.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  for (int row = 1; row <= 6; row++)
+  {
+    for (int col = 0; col < row; col++)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
+}
